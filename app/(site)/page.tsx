583217adf5856a6e391e8a9d3becbfa8a352b4d1
Mostafa Taman <mostafa.taman.me@gmail.com>
2023-11-04 09:12:18 +0200

    install shadcn ui, clean up & add eslint rules

---
+++ app/(site)/page.tsx
@@ -0,0 +1,10 @@
+
+const Home: React.FC = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hello World
+    </main>
+  );
+};
+
+export default Home;
