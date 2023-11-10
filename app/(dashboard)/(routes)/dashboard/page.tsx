e23db1ba3f4a5eb16ff18bbab163844fe39815d8
Mostafa Taman <mostafa.taman.me@gmail.com>
2023-11-10 15:31:39 +0200

    fix some styles in dashboard & settings pages

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import { MAX_FREE_LIMIT, tools } from "@/constants";
+
+import { ArrowRight } from "lucide-react";
+import { Card } from "@/components/ui/card";
 import GradientText from "@/components/general/GradientText";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
 const Dashboard: React.FC = () => {
+    const router = useRouter();
+
     return (
         <div>
             <div className="mb-8 space-y-4">
@@ -8,8 +18,24 @@ const Dashboard: React.FC = () => {
                     Explore the power of <GradientText text="A.I" />
                 </h2>
                 <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-                    All of your AI needs in one place. Start exploring for free now.
+                    All of your AI needs in one place. Start exploring now ... you get {MAX_FREE_LIMIT} free generations.
                 </p>
+            </div>
+
+            <div className="px-2 md:px-20 lg:px-32 grid grid-cols-2 gap-4">
+                {
+                    tools.map((tool) => <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-2 py-4 hover:p-4 bg-muted flex items-center justify-between hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black  transition cursor-pointer">
+                        <div className="flex items-center gap-x-2">
+                            <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                                <tool.icon className={cn("w-8 h-8", tool.color)} />
+                            </div>
+                            <div className="font-semibold">
+                                {tool.label}
+                            </div>
+                        </div>
+                        <ArrowRight className="w-5 h-5" />
+                    </Card>)
+                }
             </div>
         </div>
     );
--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -16,12 +16,16 @@ const Settings = async () => {
                 bgColor="bg-sky-700/10"
             />
 
-            <div className="px-4 lg:px-8 space-y-4">
-                <div className="text-muted-foreground text-sm">
-                    {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
-                </div>
 
-                <SubscriptionBtn isPro={isPro} />
+            <div className="px-4 mx-8 lg:px-8 space-y-4 content-center">
+
+                <div className="grid gap-4 grid-cols-2 items-center">
+                    <div className="text-muted-foreground">
+                        {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
+                    </div>
+
+                    <SubscriptionBtn isPro={isPro} />
+                </div>
             </div>
         </div>
     );
